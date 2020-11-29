b9f626faa2f4905c7d5bd8653c4e7a51dda79ca1
sam7073 <sam7073@naver.com>
2020-11-30 00:29:57 +0900

    11/30 notebook finished

--- src/Routes/Dv1.js
+++ src/Routes/Dv1.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import Chart from 'react-google-charts';
 
 export function Dv1(props) {
-  return <div>asd1</div>;
+  return (
+    <div>
+      <Chart
+        width={'500px'}
+        height={'300px'}
+        chartType="GeoChart"
+        data={[
+          ['City', 'Population', 'Area'],
+          ['Rome', 2761477, 1285.31],
+          ['Milan', 1324110, 181.76],
+          ['Naples', 959574, 117.27],
+        ]}
+        options={{
+          region: 'IT',
+          displayMode: 'markers',
+          colorAxis: { colors: ['green', 'blue'] },
+        }}
+        // Note: you will need to get a mapsApiKey for your project.
+        // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+        mapsApiKey="AIzaSyBRwJtOFOOM8EM7K975aeVdSTQPkUwggPw"
+        rootProps={{ 'data-testid': '2' }}
+      />
+    </div>
+  );
 }
 
 export default Dv1;
