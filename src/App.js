433e565c83babb268a6442ce78d1a88467451c31
unknown <sam7073@naver.com>
2020-12-02 19:42:23 +0900

    12/02 project cleanup

--- src/App.js
+++ src/App.js
@@ -26,15 +26,11 @@ function App() {
             <Link to="/dv2">
               <Button primary>시각화 2</Button>
             </Link>
-            <Link to="/result">
-              <Button primary>결론</Button>
-            </Link>
           </Card>
           <Card width="800px" height="700px" margin_left="50px">
             <Switch>
               <Route path="/dv1" exact component={Dv1} />
               <Route path="/dv2" component={Dv2} />
-              <Route path="/result" component={Result} />
             </Switch>
           </Card>
         </BrowserRouter>
